3db324e293accf66ac02137326cb9f5271489317
viralsir <viralsir2018@gmail.com>
2020-07-27 18:08:44 +0530

    Initial commit with flask_blog app and templetes folder.

---
+++ flask_blog.py
@@ -0,0 +1,33 @@
+from  flask import Flask,render_template
+
+app=Flask(__name__)
+
+posts=[
+    {
+        "author":"vimal shah",
+        "title":"First post",
+        "content":"first post content ",
+        "date_posted":" 12 feb 2020"
+    }
+    ,
+    {
+        "author":"viren shah",
+        "title":"second post",
+        "content":"second post content ",
+        "date_posted":" 12 feb 2020"
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def Home():
+    return render_template("home.html",posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
