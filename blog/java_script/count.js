ca961ca641f794e380fd3b802046069de763434a
viralsir <viralsir2018@gmail.com>
2020-10-06 18:49:17 +0530

    setinterval and taskapp added

--- blog/java_script/count.js
+++ blog/java_script/count.js
@@ -4,11 +4,14 @@
                cnt++;
                document.querySelector("h1").innerHTML=cnt;
 
-               if (cnt % 5 === 0)
+              /* if (cnt % 5 === 0)
                {
                    alert(`counter is reached to ${cnt}`)
-               }
+               }*/
         }
         document.addEventListener('DOMContentLoaded',function(){
             document.querySelector("button").onclick=count;
+
+            setInterval(count,1000);
         })
+
